e07f74e42cadec1be63b3f473e31749a2e653edb
marnaoty <32345379+marnaoty@users.noreply.github.com>
2018-05-24 15:09:23 -0400

    keep the old files away and start my new embedder

--- Function_embedder.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar  6 17:30:29 2018
-
-@author: Arnaoty
-"""
-
-
-class Function_Embedder(object):
---
+++ old/Function_embedder.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  6 17:30:29 2018
+
+@author: Arnaoty
+"""
+
+
+class Function_Embedder(object):
